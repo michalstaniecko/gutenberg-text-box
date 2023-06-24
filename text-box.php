71fd834460abf8fe3d9e28778150857b2e2690f4
Michal Staniecko <michal.staniecko@gmail.com>
2023-06-24 19:33:22 +0200

    update block metadata

--- text-box.php
+++ text-box.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Plugin Name:       Boilerplate
- * Description:       Example block scaffolded with Create Block tool.
+ * Plugin Name:       Text Box
+ * Description:       A box of text.
  * Requires at least: 6.1
  * Requires PHP:      7.0
  * Version:           0.1.0
- * Author:            The WordPress Contributors
+ * Author:            Michal Staniecko
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       boilerplate
@@ -20,7 +20,7 @@
  *
  * @see https://developer.wordpress.org/reference/functions/register_block_type/
  */
-function create_block_boilerplate_block_init() {
+function blocks_course_text_box_block_init() {
 	register_block_type( __DIR__ . '/build' );
 }
-add_action( 'init', 'create_block_boilerplate_block_init' );
+add_action( 'init', 'blocks_course_text_box_block_init' );
